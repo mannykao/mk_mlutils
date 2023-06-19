529be42a183d3ef82bc138385cbb1e880c613f88
mannyk <man962@yahoo.com>
2023-06-19 06:21:27 -0700

    version.py - define version in a tuple.

---
+++ src/version.py
@@ -0,0 +1,8 @@
+"""Library version information."""
+
+version_info = (1, 5, 0)  # (major, minor, patch)
+release_type = "-dev"
+
+version = "%s%s" % (".".join(map(str, version_info)), release_type)
+
+
